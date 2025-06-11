c83a6a59a4740cf7e95384cf9b773e26147e15ee
MarcosPucci <marcospucci2@gmail.com>
2025-06-11 20:28:21 -0300

    organizo archivos y cambio rutas

--- videoJuegos/js/scriptLogin.js
+++ videoJuegos/js/scriptLogin.js
@@ -4,9 +4,19 @@ const btnIngresar = document.getElementById("btn-ingresar");
 let nombreUsuario = "";
 
 btnIngresar.addEventListener("click", () =>{
-    nombreUsuario = inputNombre.value;
+    nombreUsuario = inputNombre.value.trim();
+    if(!verificarNombreIngresado(nombre)){
+        nombreUsuario = "Usuario anonimo";
+    };
     window.location.href = "homeCliente.html";
 });
+
+function verificarNombreIngresado(nombre){
+    if(nombre != ""){
+        return true;
+    }
+    return false;
+};
 
 export function obtenerNombre(){
     return nombreUsuario;
