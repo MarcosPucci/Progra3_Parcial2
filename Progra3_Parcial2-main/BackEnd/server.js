adaf0a1fb6e46ffd0e4949c352ccbc626303f02d
marcosfernandezzz <159031833+marcosfernandezzz@users.noreply.github.com>
2025-06-22 18:42:59 -0300

    creo las variables de entorno, la carpeta config con la configuracion de la DB y conecto la DB en server.js

---
+++ Progra3_Parcial2-main/BackEnd/server.js
@@ -0,0 +1,102 @@
+import express from "express"
+import path from "path"
+import url from "url"
+import cors from "cors"
+import sequelize from "./config/db-sequelize.js";
+import envs from "./config/envs.js";
+
+// Imports de las rutas
+import productRoutes from "./routes/products.route.js"
+/* import salesRoutes from "./routes/sales.route.js" */
+//import authRoutes from "./routes/auth.route.js" -> crear modulo de atentificacion admins
+
+
+//Inicio de servidor
+const app = express()
+
+//Rutas
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+//Config
+app.set('PORT', envs.port || 5000)
+
+const incializeConnection = async () => {
+  try {
+    await sequelize.sync()
+    console.log("Database syncronized");
+  } catch(err) {
+    console.error(err);
+  }
+}
+
+//Middlewares
+app.use(cors()) //Permite peticiones desde otros dominios (asi no tira error)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Servir archivos estáticos (HTML, CSS, JS, imágenes)
+// Todo lo que esté en la carpeta 'public' será accesible desde el navegador
+app.use(express.static(path.join(__dirname,'..', 'frontend')));
+
+
+// RUTAS DE LA API (para que el frontend pueda obtener/enviar datos)
+app.use("/api/products", productRoutes) // Rutas para productos
+/* app.use("/api/sales", salesRoutes) */ // Rutas para ventas
+/* app.use("/api/auth", authRoutes) */ // Rutas para autenticación
+
+
+// RUTAS PARA CLIENTES (página principal)
+app.get("/", (req, res) => {
+  // Esta es la página que ven los CLIENTES en el autoservicio
+  res.sendFile(path.join(__dirname, "..","frontend", "htmlCliente", "inicioCliente.html"))
+})
+
+app.get("/home-cliente", (req, res) => {
+  // Esta es la página que ven los CLIENTES en el autoservicio
+  res.sendFile(path.join(__dirname, "..","frontend", "htmlCliente", "homeCliente.html"))
+})
+
+app.get("/carrito-cliente", (req, res) => {
+  // Esta es la página que ven los CLIENTES en el carrito
+  res.sendFile(path.join(__dirname, "..","frontend", "htmlCliente", "carrito.html"))
+})
+
+//RECORDATORIO: Faltan rutas del admin
+
+
+
+// MIDDLEWARE PARA MANEJAR RUTAS NO ENCONTRADAS
+app.use("/*splat", (req, res) => { //*splat para cuando no se encuntra una ruta.
+    res.status(404).send(`
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>404 - Página no encontrada</title>
+            <style>
+                body { font-family: sans-serif; display: flex; flex-direction: column; justify-content: center; align-items: center; min-height: 100vh; color: white; text-align: center; background-color: black;}
+                h1 { font-size: 3em; margin-bottom: 0.5em; }
+                p { font-size: 1.2em; margin-bottom: 1em; }
+                a { color:rgb(243, 105, 20); text-decoration: none; font-weight: bold; }
+                a:hover { text-decoration: underline; }
+            </style>
+        </head>
+        <body>
+            <h1>Página no encontrada</h1>
+            <p>Lo sentimos, la ruta <strong>${req.originalUrl}</strong> no existe en el servidor.</p>
+            <a href="/">Volver al inicio</a>
+        </body>
+        </html>
+    `);
+});
+
+//Listeners
+incializeConnection()
+
+app.listen(app.get("PORT"), () => {
+  console.log(`Servidor corriendo en http://localhost:${app.get("PORT")}`)
+  console.log(`Interfaz Cliente: http://localhost:${app.get("PORT")}/`)
+})
+
---
+++ Progra3_Parcial2-main/FrontEnd/js/cliente/scriptCarrito.js
@@ -0,0 +1,131 @@
+/*====================================CARRITO DEL CLIENTE============================================*/
+
+/*~~~~~~~~~~~~~ Etiquetas a usar del html ~~~~~~~~~~~~~*/
+
+const ulCarrito = document.getElementById("ul-carrito");
+const btnCarrito = document.getElementById("btn-carrito");
+const totalPagar = document.getElementById("total-pagar");
+
+/*~~~~~~~~~~~~~ Variables a usar ~~~~~~~~~~~~~*/
+
+let listCarrito = JSON.parse(localStorage.getItem("listCarrito")) || [];
+
+/*~~~~~~~~~~~~~ Funciones del carrito ~~~~~~~~~~~~~*/
+
+function addCarrito(juego) {
+    const liContenedor = document.createElement("div");
+    liContenedor.className = "list-group-item d-flex flex-column flex-sm-row align-items-center m-1 gap-3";
+
+    const imgJuego = document.createElement("img");
+    imgJuego.src = "/img/ghost.jpg";
+    imgJuego.alt = `Juego de: ${juego.titulo}`;
+    imgJuego.className = "rounded";
+    imgJuego.style.width = "100px";
+    imgJuego.style.height = "100px";
+    imgJuego.style.objectFit = "cover";
+
+    const divJuego = document.createElement("div");
+    divJuego.className = "d-flex flex-column justify-content-center text-center text-sm-start";
+
+    const tituloH3 = document.createElement("h3");
+    tituloH3.innerText = `${juego.titulo}`;
+    tituloH3.className = "fw-bold mb-1";
+
+    const spanPrecio = document.createElement("span");
+    spanPrecio.innerText = `Precio x ${juego.cantidad}: $${(juego.precio * juego.cantidad)}`;
+    spanPrecio.className = "text-muted mb-2";
+
+    const btnBorrar = document.createElement("button");
+    btnBorrar.textContent = "🗑️ Borrar";
+    btnBorrar.className = "btn btn-danger fw-bold rounded-pill px-3 py-1";
+
+    btnBorrar.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        listCarrito = listCarrito.filter(j => !(j.id === juego.id && j.categoria === juego.categoria));      
+        localStorage.setItem("listCarrito", JSON.stringify(listCarrito));
+
+        renderCarrito(listCarrito);
+    });
+
+    const inputCantidad = document.createElement("input");
+    inputCantidad.type = "number";
+    inputCantidad.min = "1";
+    inputCantidad.value = juego.cantidad;
+    inputCantidad.className = "form-control my-2 text-center";
+    
+    inputCantidad.addEventListener("change", () => {
+        const nuevaCantidad = parseInt(inputCantidad.value);
+        if (nuevaCantidad > 0) {
+            juego.cantidad = nuevaCantidad;
+            localStorage.setItem("listCarrito", JSON.stringify(listCarrito));
+            renderCarrito(listCarrito);
+            mostrarCantidadEnCarrito();
+        };
+    });
+    inputCantidad.addEventListener("keyup", () =>{
+      if(inputCantidad.value === 0){
+        inputCantidad.value = 1;
+        juego.cantidad = parseInt(inputCantidad.value);
+        localStorage.setItem("listCarrito", JSON.stringify(listCarrito));
+        renderCarrito(listCarrito);
+        mostrarCantidadEnCarrito()
+      };
+    });
+    inputCantidad.addEventListener("blur", () =>{
+      if(inputCantidad.value === "" || inputCantidad.value === "0"){
+        inputCantidad.value = 1;
+        juego.cantidad = parseInt(inputCantidad.value);
+        localStorage.setItem("listCarrito", JSON.stringify(listCarrito));
+        renderCarrito(listCarrito);
+        mostrarCantidadEnCarrito()
+      };
+    });
+
+    divJuego.appendChild(tituloH3);
+    divJuego.appendChild(spanPrecio);
+    divJuego.appendChild(inputCantidad);
+    divJuego.appendChild(btnBorrar);
+
+    liContenedor.appendChild(imgJuego);
+    liContenedor.appendChild(divJuego);
+
+    return liContenedor;
+};
+
+function renderCarrito(juegosEnCarrito){
+    ulCarrito.innerHTML = "";
+    juegosEnCarrito.forEach(juego => {
+        ulCarrito.appendChild(addCarrito(juego));
+    });
+    mostrarCantidadEnCarrito();
+    totalPagar.innerHTML = "";
+    totalPagar.innerHTML = mostrarTotalPagar()
+};
+
+function mostrarCantidadEnCarrito(){
+  let cantidad = 0;
+
+  listCarrito.forEach(juego =>{
+    cantidad += juego.cantidad;
+  });
+  btnCarrito.textContent = `🛒 Carrito: ${cantidad}`;
+};
+
+function mostrarTotalPagar(){
+    let total = 0;
+    listCarrito.forEach(j =>{
+        total += (j.precio * j.cantidad);
+    });
+    return total;
+};
+
+function initCarrito() {
+  mostrarCantidadEnCarrito();
+  if (!ulCarrito){    
+    return;
+  }
+  renderCarrito(listCarrito);
+}
+
+initCarrito();
